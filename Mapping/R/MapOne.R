75f5e4ba64aa376054419f6703217a4c20f71fcb
RobHayward <robohay@gmail.com>
2015-07-05 09:29:24 +0100

    Updated the first two exercises of Mapping.  Does not seem particularly useful

--- Mapping/R/MapOne.R
+++ Mapping/R/MapOne.R
@@ -17,8 +17,30 @@ data("df_state_demographics")
 state_values = df_state_demographics[df_state_demographics$region == "missouri", 
                                      c("percent_white", "percent_black")]
 
-data(df_country_demographics)
-country_values = df_country_demographics[df_country_demographics$region == 
+data(df_county_demographics)
+county_values = df_county_demographics[df_county_demographics$region == 
                 29189, c("percent_white", "percent_black")]
                                      
-library(ch)
+library(choroplethrZip)
+data("df_zip_demographics")
+zip_values = df_zip_demographics[df_zip_demographics$region == "63135", 
+                c("percent_white", "percent_black")]
+
+# now create a single data.frame for the values
+
+df = data.frame(
+  region = c("state", "state", "county", "county", "zip", "zip"), 
+  metric = c("percent_white", "percent_black"), 
+  percent = c(state_values[1, "percent_white"], 
+              state_values[1, "percent_black"], 
+              county_values[1, "percent_white"],
+              county_values[1, "percent_black"],
+              zip_values[1, "percent_white"], 
+              zip_values[1, "percent_black"]))
+# plot
+
+library(ggplot2)
+ggplot(df, aes(region, percent,fill = metric)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  ggtitle("Demographics of the State, County and ZCTA\n of Fergusson, 
+          Missouri")
